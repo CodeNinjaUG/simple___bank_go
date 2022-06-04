703b1ec9019592d25746c5c03e4384ad706d8c76
codeninjaug <segayiandrew@gmail.com>
2022-06-04 19:20:51 +0300

    get accounts

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 	"time"
 
@@ -57,3 +58,34 @@ func TestUpdateAccount(t *testing.T) {
 	require.Equal(t, account1.Currency, account2.Currency)
 	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
+
+
+func TestDeleteAccount(t *testing.T){
+	account1 := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(),account1.ID)
+	require.NoError(t, err)
+   
+	account2, err := testQueries.GetAccount(context.Background(),account1.ID)
+	require.Error(t,err)
+	require.EqualError(t,err,sql.ErrNoRows.Error())
+	require.Empty(t,account2)
+}
+
+func TestListAccounts(t *testing.T){
+  for i:= 0; i<10; i++{
+	  createRandomAccount(t)
+  }
+  arg := GetAccountsParams{
+     Limit: 5,
+	 Offset:5,
+  }
+  accounts ,err := testQueries.GetAccounts(context.Background(), arg)
+  require.NoError(t,err)
+  require.Len(t,accounts,5)
+  
+  for _, account := range accounts {
+      require.NotEmpty(t,account)
+  }
+}
+
+ 
