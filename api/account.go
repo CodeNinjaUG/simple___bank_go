48ec9c3ac53d82d4f4c922684513eeec5f0aefe6
codeninjaug <segayiandrew@gmail.com>
2022-06-13 19:12:16 +0300

    removed the log

--- api/account.go
+++ api/account.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"database/sql"
-	"log"
 	"net/http"
 
 	db "github.com/codeninjaug/simple_bank/db/sqlc"
@@ -28,14 +27,14 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	}
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
-		if pqErr, ok := err.(*pq.Error); ok{
+		if pqErr, ok := err.(*pq.Error); ok {
 			switch pqErr.Code.Name() {
-			case "foreign_key_violation","unique_violation":
+			case "foreign_key_violation", "unique_violation":
 				ctx.JSON(http.StatusForbidden, errorResponse(err))
 				return
 			}
 			//ctx.JSON(http.StatusForbidden, errorResponse(pqErr))
-		    //return
+			//return
 		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
